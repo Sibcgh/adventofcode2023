817317d977a25fa2761e4a98e8ece63f8ea428c3
Sibghat.Shah <sibghat.shah01@gmail.com>
2023-12-09 00:57:51 -0800

    simplified

--- day_9.py
+++ day_9.py
@@ -6,22 +6,11 @@
 # extend this to do it for every line 
 # test = "0 3 6 9 12 15"
 def calculate_next(vals):
-  # base case
+  # base case, lets you check if all values in an iterator are true
   if all(x == 0 for x in vals):
     return 0
 
   #  zip allows you to pairwise quickly
-  diffs = [y-x for x,y in zip(vals, vals[1:])]
-  last_elem = calculate_next(diffs)
-  return last_elem + vals[-1]
-
-
-def calculate_next_reverse(vals):
-  vals = vals[::-1]
-  # base case
-  if all(x == 0 for x in vals):
-    return 0
-
   diffs = [y-x for x,y in zip(vals, vals[1:])]
   last_elem = calculate_next(diffs)
   return last_elem + vals[-1]
@@ -33,9 +22,8 @@ res = 0
 res2 = 0
 
 for line in l:
-
   res += calculate_next(line)
-  res2+= calculate_next_reverse(line)
+  res2+= calculate_next(line[::-1])
 print(res, res2)
 
 
